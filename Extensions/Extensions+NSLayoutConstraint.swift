ff4cbe683f551a797ec593ce47f7a60fd9f0a1ee
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-02-14 12:55:11 +0300

    Add files via upload

---
+++ Extensions/Extensions+NSLayoutConstraint.swift
@@ -0,0 +1,24 @@
+//
+//  Extensions+NSLayoutConstraint.swift
+//  NTVK
+//
+//  Created by Nikita Teslyuk on 11/02/2019.
+//  Copyright © 2019 Tesnik. All rights reserved.
+//
+
+import UIKit
+
+extension NSLayoutConstraint {
+  static func quadroAspect(on view: UIView) -> NSLayoutConstraint {
+    return NSLayoutConstraint.init(item: view, attribute: .height, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0)
+  }
+  
+  static func contraints(withNewVisualFormat vf: String, dict: [String : Any]) -> [NSLayoutConstraint] {
+    let separatedArray = vf.split(separator: ",")
+    switch separatedArray.count {
+    case 1: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict)
+    case 2: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict) + NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[1])", options: [], metrics: nil, views: dict)
+    default: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict)
+    }
+  }
+}
