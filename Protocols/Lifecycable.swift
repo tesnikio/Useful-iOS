3ebd6609e295b469703c16a547fcff9fbe929046
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-03-18 23:10:56 +0300

    Update Lifecycable.swift

--- Protocols/Lifecycable.swift
+++ Protocols/Lifecycable.swift
@@ -1,11 +1,3 @@
-//
-//  Lifecycable.swift
-//  Guess the Place!
-//
-//  Created by Nikita Teslyuk on 15/03/2019.
-//  Copyright © 2019 Tesnik. All rights reserved.
-//
-
 import Foundation
 
 @objc protocol Lifecycable {
