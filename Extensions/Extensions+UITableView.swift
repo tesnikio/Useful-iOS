fce7884596131d939ff3b9a9218e2752618d15c4
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-03-19 12:42:34 +0300

    Update Extensions+UITableView.swift

--- Extensions/Extensions+UITableView.swift
+++ Extensions/Extensions+UITableView.swift
@@ -1,23 +1,7 @@
-//
-//  Extensions+UITableView.swift
-//  NTVK
-//
-//  Created by Nikita Teslyuk on 06/03/2019.
-//  Copyright © 2019 Tesnik. All rights reserved.
-//
-
 import UIKit
 
 extension UITableView {
-  func dequeueReusableCell(withModel model: CellViewAnyModel, for indexPath: IndexPath) -> UITableViewCell {
-    let identifier = String(describing: type(of: model).cellAnyType)
-    let cell = self.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
-    
-    model.setupAny(cell: cell)
-    return cell
-  }
-  
-  func register(nibModels: [CellViewAnyModel.Type]) {
+  func register(nibModels: [CellType.Type]) {
     for model in nibModels {
       let identifier = String(describing: model.cellAnyType)
       let nib = UINib(nibName: identifier, bundle: nil)
