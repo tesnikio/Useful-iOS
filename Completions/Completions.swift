b13332084299c5d2aeec8ecf46c3917df574d422
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-04-13 18:46:16 +0300

    Update Completions.swift

--- Completions/Completions.swift
+++ Completions/Completions.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-typealias ItemClosure<T> = ((T) -> Void)
-typealias OptionalItemClosure<T> = ((T?) -> Void)
-typealias VoidClosure = (() -> Void)
+typealias ItemClosure<T> = (T) -> Void
+typealias OptionalItemClosure<T> = (T?) -> Void
+typealias VoidClosure = () -> Void
