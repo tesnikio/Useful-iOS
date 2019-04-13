8e708f5950606f0018a4b2b6455f2317928aca55
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-04-13 18:45:13 +0300

    Update Extensions+Encodable.swift

--- Extensions/Extensions+Encodable.swift
+++ Extensions/Extensions+Encodable.swift
@@ -3,6 +3,6 @@ import Foundation
 extension Encodable {
   var dictionary: [String: Any]? {
     guard let data = try? JSONEncoder().encode(self) else { return nil }
-    return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
+    return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).compactMap { $0 as? [String: Any] }
   }
 }
