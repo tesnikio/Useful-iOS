0a86641b702140a5230fccbf452ac6fc54bcca1c
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-07-21 16:12:06 +0300

    Update Extensions+UITableView.swift

--- Extensions/Extensions+UITableView.swift
+++ Extensions/Extensions+UITableView.swift
@@ -1,11 +1,11 @@
 import UIKit
 
 extension UITableView {
-  func register(nibModels: [CellType.Type]) {
-    for model in nibModels {
-      let identifier = String(describing: model.cellAnyType)
-      let nib = UINib(nibName: identifier, bundle: nil)
-      self.register(nib, forCellReuseIdentifier: identifier)
+    func register(nibModels: [CellType.Type]) {
+        for model in nibModels {
+            let identifier = String(describing: model.cellAnyType)
+            let nib = UINib(nibName: identifier, bundle: nil)
+            self.register(nib, forCellReuseIdentifier: identifier)
+        }
     }
-  }
 }
