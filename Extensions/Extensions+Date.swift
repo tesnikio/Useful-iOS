bd6a370dd3074ec3d8c1ba9314b3aac6f69ea1e5
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-07-21 16:24:16 +0300

    Update Extensions+Date.swift

--- Extensions/Extensions+Date.swift
+++ Extensions/Extensions+Date.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 extension Date {
-  static func timeFromUnix(unixTime: Double) -> String {
-    let timeInSeconds = TimeInterval(unixTime)
-    let date = Date(timeIntervalSince1970: timeInSeconds)
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "HH:mm"
-    return dateFormatter.string(from: date)
-  }
+    static func timeFromUnix(unixTime: Double) -> String {
+        let timeInSeconds = TimeInterval(unixTime)
+        let date = Date(timeIntervalSince1970: timeInSeconds)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        return dateFormatter.string(from: date)
+    }
 }
