e123b0fabf1011e0d67ac176d25b7410feb144c3
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-07-21 16:11:44 +0300

    Update Extensions+UIAlertController.swift

--- Extensions/Extensions+UIAlertController.swift
+++ Extensions/Extensions+UIAlertController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
 extension UIViewController {
-  func showAlert(with title: String, and message: String) {
-    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-    alertController.addAction(okAction)
-    present(alertController, animated: true, completion: nil)
-  }
+    func showAlert(with title: String, and message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
+
