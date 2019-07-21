ab888f82884740e69facb103cb74510aaeb6adba
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-07-21 16:24:01 +0300

    Update Extensions+Decodable.swift

--- Extensions/Extensions+Decodable.swift
+++ Extensions/Extensions+Decodable.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 extension Decodable {
-  init(from: Any) throws {
-    let data = try JSONSerialization.data(withJSONObject: from, options: .prettyPrinted)
-    let decoder = JSONDecoder()
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:sszzz"
-    decoder.dateDecodingStrategy = .formatted(dateFormatter)
-    self = try decoder.decode(Self.self, from: data)
-  }
+    init(from: Any) throws {
+        let data = try JSONSerialization.data(withJSONObject: from, options: .prettyPrinted)
+        let decoder = JSONDecoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:sszzz"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        self = try decoder.decode(Self.self, from: data)
+    }
 }
