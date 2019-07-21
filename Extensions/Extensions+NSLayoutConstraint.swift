bddc8a94ce1d6dec921e6e886bd5bc1d511eacab
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-07-21 16:23:29 +0300

    Update Extensions+NSLayoutConstraint.swift

--- Extensions/Extensions+NSLayoutConstraint.swift
+++ Extensions/Extensions+NSLayoutConstraint.swift
@@ -1,16 +1,16 @@
 import UIKit
 
 extension NSLayoutConstraint {
-  static func quadroAspect(on view: UIView) -> NSLayoutConstraint {
-    return NSLayoutConstraint.init(item: view, attribute: .height, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0)
-  }
-  
-  static func contraints(withNewVisualFormat vf: String, dict: [String: Any]) -> [NSLayoutConstraint] {
-    let separatedArray = vf.split(separator: ",")
-    switch separatedArray.count {
-    case 1: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict)
-    case 2: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict) + NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[1])", options: [], metrics: nil, views: dict)
-    default: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict)
+    static func quadroAspect(on view: UIView) -> NSLayoutConstraint {
+        return NSLayoutConstraint.init(item: view, attribute: .height, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0)
     }
-  }
+    
+    static func contraints(withNewVisualFormat vf: String, dict: [String: Any]) -> [NSLayoutConstraint] {
+        let separatedArray = vf.split(separator: ",")
+        switch separatedArray.count {
+        case 1: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict)
+        case 2: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict) + NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[1])", options: [], metrics: nil, views: dict)
+        default: return NSLayoutConstraint.constraints(withVisualFormat: "\(separatedArray[0])", options: [], metrics: nil, views: dict)
+        }
+    }
 }
