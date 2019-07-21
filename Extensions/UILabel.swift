0cbb2ae48359503a88e0fb6e569c943ee99c3060
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-07-21 16:22:38 +0300

    Update UILabel.swift

--- Extensions/UILabel.swift
+++ Extensions/UILabel.swift
@@ -1,12 +1,12 @@
 import UIKit
 
 extension UILabel {
-  convenience init(text: String?, font: UIFont? = UIFont.systemFont(ofSize: 14), textColor: UIColor = .black, textAlignment: NSTextAlignment = .left, numberOfLines: Int = 1) {
-    self.init()
-    self.text = text
-    self.font = font
-    self.textColor = textColor
-    self.textAlignment = textAlignment
-    self.numberOfLines = numberOfLines
-  }
+    convenience init(text: String?, font: UIFont? = UIFont.systemFont(ofSize: 14), textColor: UIColor = .black, textAlignment: NSTextAlignment = .left, numberOfLines: Int = 1) {
+        self.init()
+        self.text = text
+        self.font = font
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+        self.numberOfLines = numberOfLines
+    }
 }
