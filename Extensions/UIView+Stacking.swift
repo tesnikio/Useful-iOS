252154a13063545afbbec8f3726f88007c680bae
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-07-21 16:23:03 +0300

    Update UIView+Stacking.swift

--- Extensions/UIView+Stacking.swift
+++ Extensions/UIView+Stacking.swift
@@ -1,46 +1,46 @@
 import UIKit
 
 extension UIView {
-  @discardableResult
-  func withSize(size: CGSize) -> UIView {
-    translatesAutoresizingMaskIntoConstraints = false
-    widthAnchor.constraint(equalToConstant: size.width).isActive = true
-    heightAnchor.constraint(equalToConstant: size.height).isActive = true
-    return self
-  }
-  
-  @discardableResult
-  func withHeight(height: CGFloat) -> UIView {
-    translatesAutoresizingMaskIntoConstraints = false
-    heightAnchor.constraint(equalToConstant: height).isActive = true
-    return self
-  }
-  
-  @discardableResult
-  func withWidth(_ width: CGFloat) -> UIView {
-    translatesAutoresizingMaskIntoConstraints = false
-    widthAnchor.constraint(equalToConstant: width).isActive = true
-    return self
-  }
-  
-  @discardableResult
-  func withBorder(width: CGFloat, color: UIColor) -> UIView {
-    layer.borderWidth = width
-    layer.borderColor = color.cgColor
-    return self
-  }
+    @discardableResult
+    func withSize(size: CGSize) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func withHeight(height: CGFloat) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func withWidth(_ width: CGFloat) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func withBorder(width: CGFloat, color: UIColor) -> UIView {
+        layer.borderWidth = width
+        layer.borderColor = color.cgColor
+        return self
+    }
 }
 
 extension UIEdgeInsets {
-  static func allSides(side: CGFloat) -> UIEdgeInsets {
-    return .init(top: side, left: side, bottom: side, right: side)
-  }
+    static func allSides(side: CGFloat) -> UIEdgeInsets {
+        return .init(top: side, left: side, bottom: side, right: side)
+    }
 }
 
 extension UIImageView {
-  convenience init(image: UIImage?, contentMode: UIView.ContentMode = .scaleAspectFill) {
-    self.init(image: image)
-    self.contentMode = contentMode
-    self.clipsToBounds = true
-  }
+    convenience init(image: UIImage?, contentMode: UIView.ContentMode = .scaleAspectFill) {
+        self.init(image: image)
+        self.contentMode = contentMode
+        self.clipsToBounds = true
+    }
 }
