132258139766a3d1cbb827073862b00f178e73cb
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-07-23 13:53:34 +0300

    Update NibLoadable.swift

--- Protocols/NibLoadable.swift
+++ Protocols/NibLoadable.swift
@@ -1,25 +1,25 @@
 import UIKit
 
 protocol NibLoadable: class {
-  static var nib: UINib { get }
+    static var nib: UINib { get }
 }
 
 extension NibLoadable {
-  static var nib: UINib {
-    return UINib(nibName: name, bundle: Bundle.init(for: self))
-  }
-  
-  static var name: String {
-    return String(describing: self)
-  }
+    static var nib: UINib {
+        return UINib(nibName: name, bundle: Bundle.init(for: self))
+    }
+    
+    static var name: String {
+        return String(describing: self)
+    }
 }
 
 extension NibLoadable where Self: UIView {
-  static func loadFromNib() -> Self {
-    guard let view = nib.instantiate(withOwner: nil, options: nil).first as? Self else {
-      fatalError()
+    static func loadFromNib() -> Self {
+        guard let view = nib.instantiate(withOwner: nil, options: nil).first as? Self else {
+            fatalError()
+        }
+        
+        return view
     }
-    
-    return view
-  }
 }
