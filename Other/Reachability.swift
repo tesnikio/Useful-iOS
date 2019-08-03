5aada23d69903cfd2843409cda23a1088888c26d
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-08-03 20:46:02 +0300

    Add files via upload

---
+++ Other/Reachability.swift
@@ -0,0 +1,19 @@
+import Foundation
+import SystemConfiguration
+
+public class Reachability {
+    public class var isConnectedToNetwork: Bool {
+        var zeroAddress = sockaddr()
+        zeroAddress.sa_len = UInt8(MemoryLayout<sockaddr>.size)
+        zeroAddress.sa_family = sa_family_t(AF_INET)
+
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }) else { return false }
+
+        var flags = SCNetworkReachabilityFlags()
+        guard SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) else { return false }
+
+        return flags.contains(.reachable) && !flags.contains(.connectionRequired)
+    }
+}
