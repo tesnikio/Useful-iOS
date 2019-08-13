e8fa0b4dc784a5f384512aed8634f93a919abc2e
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-08-13 18:28:34 +0300

    Add files via upload

---
+++ Other/SeparatorView.swift
@@ -0,0 +1,47 @@
+//
+//  SeparatorView.swift
+//  NTAppStore
+//
+//  Created by Nikita Teslyuk on 13/08/2019.
+//  Copyright © 2019 Tesnik. All rights reserved.
+//
+
+import UIKit
+
+internal final class SeparatorView: UIView {
+    
+    // MARK: Lifecycle
+    internal init() {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        setUpConstraints()
+    }
+    
+    internal required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Internal
+    internal override var intrinsicContentSize: CGSize {
+        return CGSize(width: width, height: width)
+    }
+    
+    internal var color: UIColor {
+        get { return backgroundColor ?? .clear }
+        set { backgroundColor = newValue }
+    }
+    
+    internal var width: CGFloat = 1 {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    // MARK: Private
+    private func setUpConstraints() {
+        setContentHuggingPriority(.defaultLow, for: .horizontal)
+        setContentHuggingPriority(.defaultLow, for: .vertical)
+        setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+    }
+    
+}
+
