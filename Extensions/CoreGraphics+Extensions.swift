61cab468a5c9803f943870bb52af4d83a6cc0820
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-08-16 17:50:31 +0300

    Update CoreGraphics+Extensions.swift

--- Extensions/CoreGraphics+Extensions.swift
+++ Extensions/CoreGraphics+Extensions.swift
@@ -1,11 +1,3 @@
-//
-//  CoreGraphics+Extensions.swift
-//  Utils
-//
-//  Created by Dmitry Trimonov on 18/03/2019.
-//  Copyright © 2019 Dmitry Trimonov. All rights reserved.
-//
-
 import CoreGraphics
 
 extension CGSize {
